621d61f18b9ffe74327b6bdd28911cc7e640a46b
HZp <88123889+GGHZp@users.noreply.github.com>
2024-03-15 09:23:26 +0800

    Update scanLogs.bat

--- scanLogs.bat
+++ scanLogs.bat
@@ -5,8 +5,10 @@ set "rred=[91m"
 set "n=&"
 set "rarrow=>"
 set "ttxtend=[0m"
-
 cd "%localappdata%\temp\ROTWETemp"
+timeout /t 1 /nobreak>nul
 echo ms = WScript.Arguments(0) > sleep.vbs
 echo WScript.Sleep ms >> sleep.vbs
+timeout /t 1 /nobreak>nul
+:: --------------------------------
 
